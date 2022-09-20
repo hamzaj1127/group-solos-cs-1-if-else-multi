f184e4a433f4ef991c5f011d89f724eb457ed9b4
hamzaj1127 <89408188+hamzaj1127@users.noreply.github.com>
2022-09-20 08:27:49 -0400

    Add files via upload

---
+++ Iecs1sy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Group_Project_C_solo
+{
+    internal class Iecs1sy
+    {
+        static void Main(string[] args)
+        {
+            int Count = 70;
+
+
+            while (Count < 100)
+            {
+                Count+= 1;
+                if (Count <= 75)
+                    Console.WriteLine($"{Count} is less than 75");
+                else
+                    Console.WriteLine($"{Count} is greater than 75");
+
+            }
+           
+
+
+        }
+    }
+}
---
+++ multithread.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+namespace threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread uno = new Thread(dos);
+            uno.Start();
+            Console.WriteLine("uno is working");
+            Console.ReadKey();
+        }
+        static void dos()
+        {
+            Console.WriteLine("dos is working");
+        }
+    }
+}
